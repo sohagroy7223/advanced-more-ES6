e1d36cc13eaf186825e86165feca19a462819bf4
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-28 11:39:57 +0600

    addad sort loop function map

---
+++ map.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// const newArray = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   const element = numbers[i];
+//   const sum = element + 1;
+//   newArray.push(sum);
+// }
+// console.log(newArray);
+
+const square = numbers.map((value) => value * 2);
+console.log(square);
