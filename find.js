0c125cbbb20a6334ea0e9195f42a619efedb04fc
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-28 23:12:03 +0600

    addad find function this function can find only on property

--- find.js
+++ find.js
@@ -1,18 +1,13 @@
-const produced = [
+const allModel = [
   { id: 1, name: "iphone", model: "iphone17", color: "gold", price: 1300 },
   { id: 2, name: "xiaomi", model: "xiaomi not 13", color: "blue", price: 200 },
   { id: 3, name: "samsung", model: "samsung s25", color: "blue", price: 1100 },
   { id: 4, name: "iphone", model: "iphone13", color: "gold", price: 1000 },
   { id: 5, name: "xiaomi", model: "xiaomi not 8", color: "blue", price: 100 },
 ];
-produced.forEach((product) => {
-  if (product.color === "gold") {
-    // console.log(product);
-  }
-});
 
-produced.forEach((element) => {
-  if (element.price >= 1000) {
-    // console.log(element);
-  }
-});
+const presell = allModel.find((p) => p.id === 4);
+// console.log(presell);
+
+const thirdModel = allModel.find((s) => s.id === 3);
+console.log(thirdModel);
