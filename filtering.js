a3e659e42b0f34f290887f3835b0ce3d31fc401c
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-28 22:57:57 +0600

    addad filter function in this file

--- filtering.js
+++ filtering.js
@@ -1,18 +1,14 @@
-const produced = [
+const products = [
   { id: 1, name: "iphone", model: "iphone17", color: "gold", price: 1300 },
   { id: 2, name: "xiaomi", model: "xiaomi not 13", color: "blue", price: 200 },
   { id: 3, name: "samsung", model: "samsung s25", color: "blue", price: 1100 },
   { id: 4, name: "iphone", model: "iphone13", color: "gold", price: 1000 },
   { id: 5, name: "xiaomi", model: "xiaomi not 8", color: "blue", price: 100 },
 ];
-produced.forEach((product) => {
-  if (product.color === "gold") {
-    // console.log(product);
-  }
-});
 
-produced.forEach((element) => {
-  if (element.price >= 1000) {
-    console.log(element);
+const sameColor = products.filter((product) => {
+  if (product.color === "blue") {
+    return product;
   }
 });
+// console.log(sameColor);
