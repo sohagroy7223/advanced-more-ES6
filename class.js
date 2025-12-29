776ac1d7ac7985cf2fa210e738945c6ad8a01bbe
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-29 13:19:07 +0600

    adadd new fashion

--- class.js
+++ class.js
@@ -1,19 +1,40 @@
-class product {
-  constructor(name, price, color) {
-    this.malik = "sohag roy";
-    this.name = name;
+// class product {
+//   constructor(name, price, color) {
+//     this.malik = "sohag roy";
+//     this.name = name;
+//     this.price = price;
+//     this.color = color;
+//   }
+
+//   details() {
+//     console.log(`this is ${this.name} product ar details`);
+//   }
+// }
+
+// const iphone = new product("iphone", 1200, "blue");
+// iphone.details();
+// console.log(iphone);
+
+// const xiaomi = new product("redmi", 1000, " blue");
+// xiaomi.details();
+// console.log(xiaomi);
+
+class clothing {
+  constructor(what, price, color) {
+    this.woner = "sohga fashion";
+    this.brand = what;
     this.price = price;
     this.color = color;
   }
-
   details() {
-    console.log(`this is ${this.name} product ar details`);
+    console.log(`this ${this.brand} full details`);
   }
 }
 
-const iphone = new product("iphone", 1200, "blue");
-iphone.details();
-console.log(iphone);
+const pant = new clothing("calvin", 800, "naviblue");
+pant.details();
+console.log(pant);
 
-const xiaomi = new product("redmi", 1000, " blue");
-console.log(xiaomi);
+const hodi = new clothing("Hodi", 1200, "sky-blue");
+hodi.details();
+console.log(hodi);
