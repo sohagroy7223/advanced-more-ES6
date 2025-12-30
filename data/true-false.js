a6875c252111694cd6c68d0e668185ef0d2ec1a4
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-30 18:00:08 +0600

    addad true and false check

---
+++ data/true-false.js
@@ -0,0 +1,19 @@
+const call = undefined;
+
+if (call) {
+  console.log("this is truthy value");
+} else {
+  console.log("this is falsy value");
+}
+// const call = " "; //true
+// const call = ""; //false
+// const call = "sohag"; //true
+// const call = 0;/-0 //false
+// const call = all number -/+ //true
+// const call = true;//true
+// const call = false;//false
+// const call = []; // true
+// const call = {}; // true
+// const call = function () {};// true
+// const call = null;// false
+// const call = undefined;// false
