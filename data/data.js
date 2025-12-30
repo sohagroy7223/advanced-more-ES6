f2bed175f0889adb57afaa4b59846297e6407249
sohagroy7223 <freelancersohagroy@gmail.com>
2025-12-30 16:38:56 +0600

    addad data type primitive can not change and non primitiveyou can change

---
+++ data/data.js
@@ -0,0 +1,14 @@
+// primitive data , you can change***********
+
+let person = "sohag roy";
+let newPerson = person;
+person = "hdbch";
+
+// console.log(newPerson);
+
+// non-primitive data can't change***********
+let people = { name: "sohag roy" };
+let newPeople = people;
+people.name = "hero manna";
+
+console.log(newPeople);
